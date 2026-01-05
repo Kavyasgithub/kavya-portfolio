45999a15c03f3aa3b870c299bb88cbc346d5c8b8
Kavyasgithub <142691600+Kavyasgithub@users.noreply.github.com>
2026-01-06 02:34:07 +0530

    fix: exclude sanity-studio from Next.js build to resolve deployment error

--- sanity-studio/sanity.config.ts
+++ sanity-studio/sanity.config.ts
@@ -1,6 +1,6 @@
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
-import {visionTool} from '@sanity/vision'
+// import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemaTypes'
 import project from './schemaTypes/project'
 import post from './schemaTypes/post'
